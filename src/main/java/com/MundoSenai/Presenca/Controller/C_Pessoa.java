77f5298b81b329ac3bdee48489fca0ec66de4722
Hellenduarte05 <hellenduarte25@gmail.com>
2023-08-08 17:14:58 -0300

    atualizacao de login

--- src/main/java/com/MundoSenai/Presenca/Controller/C_Pessoa.java
+++ src/main/java/com/MundoSenai/Presenca/Controller/C_Pessoa.java
@@ -1,29 +1,50 @@
 package com.MundoSenai.Presenca.Controller;
 
+import com.MundoSenai.Presenca.Model.m_Pessoa;
 import com.MundoSenai.Presenca.Service.s_Pessoa;
+import jakarta.servlet.http.HttpSession;
+import org.apache.catalina.filters.ExpiresFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@SessionAttributes("usuario")
 public class C_Pessoa {
     @GetMapping("/")
     public String helloWord(){return "Login/login";}
 
     @PostMapping("/")
-    public String postLogin(@RequestParam("usuario") String usuario, @RequestParam("senha") String senha){
-        if(s_Pessoa.getPessoaLogin(usuario,senha) == null){
+    public String postLogin(@RequestParam("usuario") String usuario, @RequestParam("senha") String senha, HttpSession session){
+        session.setAttribute("usuario", s_Pessoa.getPessoaLogin(usuario, senha));
+        if(session.getAttribute("usuario") == null){
             return "Login/login";
         }else{
-            return "Home/home";
+            return "redirect:/Home";
         }
     }
+
+    @ModelAttribute("usuario")
+    public m_Pessoa getUsuario(HttpSession session){
+      return (m_Pessoa) session.getAttribute("usuario");
+    }
+
+    @GetMapping("/Home")
+    public String getHome(@ModelAttribute("usuario") String usuario){
+        if(usuario != null){
+            //a sessao existe, redirecionar para a pagina home
+            return "Home/home";
+        }else{
+            //a sessao não existe redirecinar para a pagina de login
+            return "redirect:/";
+        }
+    }
+
     @GetMapping("/cadastro")
     public String getCadastro(){
         return "Pessoa/cadastro";
     }
+
     @PostMapping("/cadastro")
     public String postCadastro(@RequestParam("nome") String nome, @RequestParam("email") String email, @RequestParam("cpf") String cpf,
                                @RequestParam("telefone") String telefone, @RequestParam("data_nasc") String Datanasc,
--- src/main/java/com/MundoSenai/Presenca/Service/s_Pessoa.java
+++ src/main/java/com/MundoSenai/Presenca/Service/s_Pessoa.java
@@ -2,9 +2,12 @@ package com.MundoSenai.Presenca.Service;
 
 import com.MundoSenai.Presenca.Model.m_Pessoa;
 import com.MundoSenai.Presenca.Repostory.R_Pessoa;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+
+import static com.MundoSenai.Presenca.Service.NumberCleaner.cleanNumber;
 
 @Service
 public class s_Pessoa {
@@ -15,31 +18,59 @@ public class s_Pessoa {
     }
 
     public static m_Pessoa getPessoaLogin(String usuario, String senha){
-        return r_Pessoa.findByUsuarioESenha(Long.valueOf(usuario),senha);
+        usuario = NumberCleaner.cleanNumber(usuario);
+        if(usuario.equals("")){
+            return r_Pessoa.findByUsuarioESenha(null,senha);
+        }else{
+            return r_Pessoa.findByUsuarioESenha(Long.valueOf(usuario),senha);
+        }
     }
 
     public static String cadastrarPessoa(String nome, String email, String cpf, String telefone, String datanasc, String senha, String confsenha){
-          String mensagemReturn = "";
+        boolean cadastroValido = true;
+        String mensagemReturn = "";
+        telefone = NumberCleaner.cleanNumber(telefone);
+        if(telefone.equals("")){
+            telefone = null;
+        }
         if(!senha.equals(confsenha)) {
-            mensagemReturn += "A Senha e a Confirmação de Senha devem serm iguais";
-        }if(!CpfValidator.validateCPF(cpf)){
-            mensagemReturn += "CPF Inválido";
-        }if(nome == null || nome.trim() == ""){
-            mensagemReturn += "Deve ser informado o Nome";
-        }if ((email == null || email.trim() == "")
-                && (NumberCleaner.cleanNumber(telefone) == null
-                || NumberCleaner.cleanNumber(telefone).trim() == "")){
-            mensagemReturn += "e-Mail ou Telefone precisa ser informado";
-    }else{
+            mensagemReturn += "A Senha e a Confirmação de Senha devem ser iguais<br/>";
+            cadastroValido = false;
+        }
+        if(!CpfValidator.validateCPF(cpf)){
+            mensagemReturn += "CPF Inválido<br/>";
+            cadastroValido = false;
+        }
+        if(nome == null || nome.trim() == ""){
+            mensagemReturn += "Deve ser informado o Nome<br/>";
+            cadastroValido = false;
+        }
+        if ((email == null || email.trim() == "")
+                && (telefone == null)){
+            mensagemReturn += "e-Mail ou Telefone precisa ser informado<br/>";
+            cadastroValido = false;
+        }
+        if(cadastroValido){
         m_Pessoa M_Pessoa = new m_Pessoa();
         M_Pessoa.setNome(nome);
-        M_Pessoa.setCpf(Long.valueOf(NumberCleaner.cleanNumber(cpf)));
-        M_Pessoa.setTelefone(Long.valueOf(NumberCleaner.cleanNumber(telefone)));
+        M_Pessoa.setCpf(Long.valueOf(cleanNumber(cpf)));
+        if(telefone != null){
+           M_Pessoa.setTelefone(Long.valueOf(telefone));
+        }
         M_Pessoa.setEmail(email);
         M_Pessoa.setData_nasc(LocalDate.parse(datanasc));
         M_Pessoa.setSenha(senha);
-        r_Pessoa.save(M_Pessoa);
-        mensagemReturn +=  "Cadastro efetuado com sucesso";
+        try{
+            r_Pessoa.save(M_Pessoa);
+            mensagemReturn +=  "Cadastro efetuado com sucesso";
+        }catch (DataIntegrityViolationException e){
+            if(e.getMessage().contains("u_cpf")){
+                mensagemReturn += "o Cpf informado já foi cadastro";
+            }else{
+                mensagemReturn += "Erro ao cadastrar";
+            }
+            mensagemReturn += e.getMessage();
+        }
     }
      return mensagemReturn;
     }
